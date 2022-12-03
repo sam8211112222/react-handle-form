c580f5b71ba9d3ac977f17569fe5740284a9e08d
Sam Chen <sam8211112222@gmail.com>
2022-12-03 11:38:59 +0800

    modify crud function

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './App.css';
 import BookCreate from "./components/BookCreate";
 import BookList from "./components/BookList";
@@ -8,11 +8,26 @@ function App() {
 
     const [books, setBooks] = React.useState<{ id: number, title: string }[]>([])
 
-    const editBookById = (id: number, newTitle: string) => {
-        setBooks(books.map(b => b.id === id ? {...b, title: newTitle} : b))
+    const fetchBookas = async () =>{
+        const response =await axios.get('http://localhost:3001/books')
+        setBooks(response.data)
     }
 
-    const deleteBookHandler = (bookId: number) => {
+    useEffect(()=>{
+        fetchBookas()
+    },[])
+
+    const editBookById = async (id: number, newTitle: string) => {
+        const response = await axios.put(`http://localhost:3001/books/${id}`,{
+            title:newTitle
+        })
+        // ...b 表示沒變動的資料
+        // ...response.data 表示有變動過的資料
+        setBooks(books.map(b => b.id === id ? {...b, ...response.data} : b))
+    }
+
+    const deleteBookHandler = async (bookId: number) => {
+        await axios.delete(`http://localhost:3001/books/${bookId}`)
         setBooks(books.filter(b => b.id !== bookId))
     }
 
